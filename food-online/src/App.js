80c962c5507692a97407e7b508c27cf8d415f5e7
April Ma <54913304+april5622@users.noreply.github.com>
2020-11-19 20:16:59 -0800

    basic set up

---
+++ food-online/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Homepage from './components/Homepage';
+import Navigation from './components/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello</h1>
+     <Navigation/>
+     <Homepage/>
+    
+    </div>
+  );
+}
+
+export default App;
